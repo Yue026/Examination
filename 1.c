7176ba10ef9704c82b97006ed89ed5f3ed7e2dbd
douzi <2083772270@qq.com>
2024-10-15 03:17:25 +0800

    first commit

---
+++ 1.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+
+int main()
+{
+    int a[10], i, maxi, mini, sum = 0;
+    for (i = 0; i < 10; i++)
+    {
+        printf("Please input the score of a student(integer):\n");
+        scanf_s("%d", &a[i]);
+    }
+    maxi = mini = a[0];
+    for (i = 0; i < 10; i++)
+    {
+        sum += a[i];
+        if (a[i] > maxi)     
+            maxi = a[i];
+        if (a[i] < mini)
+            mini = a[i];
+    }
+    printf("max=%d min=%d\n", maxi, mini);
+    printf("aver=%f\n", (float)sum / 10);
+
+    return 0;
+} 
---
+++ 3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10]={90,61,92,79,45,67,86,95,63,79};
+    int i,j,temp;
+    for(i=0;i<9;i++)
+    {
+        for(j=0;j<9-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+        printf("%d ",a[i]);
+    return 0;
+}
